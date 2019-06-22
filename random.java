8152be4cf9cc75132a59189342d71670e1e5e425
nerdstep710 <49726028+nerdstep710@users.noreply.github.com>
2019-06-22 14:11:56 -0500

    Update random.java

--- random.java
+++ random.java
@@ -1,6 +1,6 @@
 public class random
 {
-  public static int randomInt(int low, int high)
+  public static int randint(int low, int high)
   {
     int result = (int)(Math.random() * (high - low + 1)) + low;
     return result;
